27d1f5cdf847bc86d529ded332b1d53c4b2b1be4
Marat <kashaevmt@gmail.com>
2019-09-25 01:46:40 +0300

    PATCH port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require('express')
 const axios = require('axios')
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000
 
 // config.TOKEN
 // config.CHAT_ID
