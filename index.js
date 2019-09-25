d09359b466ffa2abc6908e6c66b3c886dccca602
Marat <kashaevmt@gmail.com>
2019-09-25 15:30:54 +0300

    PATCH require config file

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require('express')
 const axios = require('axios')
 const app = express()
-const config = require('./config.js')()
+const config = require('./config-example.js')()
 
 const port = process.env.PORT || 5000
 const token = process.env.TOKEN || config.TOKEN
